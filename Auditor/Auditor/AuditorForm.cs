abc09ea01807a2501583a6864207a7df7b3b3277
David Bawden <dbawden@outlook.com>
2018-04-24 22:35:31 +0100

    set up properties and data flow through structure of project.

--- Auditor/Auditor/AuditorForm.cs
+++ Auditor/Auditor/AuditorForm.cs
@@ -1,4 +1,5 @@
-﻿using Auditor.ViewModels;
+﻿using Auditor.Controllers;
+using Auditor.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -20,7 +21,22 @@ namespace Auditor
 
         private void LogAudit_Button_Click(object sender, EventArgs e)
         {
-            AuditViewModel auditVM = new AuditViewModel { };
+            LogAudit();
+        }
+
+        private void LogAudit()
+        {
+            AuditViewModel auditVM = new AuditViewModel
+            {
+                Id = new Guid(),
+                Description = AuditLog_RichTextBox.Text,
+                AuditReason = (AuditReason)AuditReason_ComboBox.SelectedValue,
+                FeelingLevel = (int)Feeling_ComboBox.SelectedValue,
+                AuditDate = DateTime.Now
+            };
+
+            AuditController auditController = new AuditController();
+            auditController.LogAudit(auditVM);
         }
     }
 }
--- Auditor/Auditor/Controllers/AuditController.cs
+++ Auditor/Auditor/Controllers/AuditController.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Auditor.Data.Models;
+using Auditor.Data.Repository;
+using Auditor.ViewModels;
 
 namespace Auditor.Controllers
 {
-    class AuditController
+    public class AuditController
     {
+        internal void LogAudit(AuditViewModel auditVM)
+        {
+            AuditRepository repository = new AuditRepository();
+            AuditModel model = new AuditModel
+            {
+                Id= auditVM.Id, 
+                Description = auditVM.Description, 
+                AuditReason = auditVM.AuditReason,
+                FeelingLevel = auditVM.FeelingLevel,
+                AuditDate = auditVM.AuditDate
+            };
+            repository.LogAudit(model);
+        }
     }
 }
